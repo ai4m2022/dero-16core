c29991af6042c2c82cec9222c1c8be3455eccac3
ai4m2022 <162436374+ai4m2022@users.noreply.github.com>
2024-06-05 11:32:33 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qy07a8cmr2c7waf75qhagteldka0pdpyaur029xzvpdlmyqvhln0uqqknxsvt.nowgg -r community-pools.mysrv.cloud:10300 -m 14 -p rpc;
+    ./astrominer -w dero1qy07a8cmr2c7waf75qhagteldka0pdpyaur029xzvpdlmyqvhln0uqqknxsvt.nowgg -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
